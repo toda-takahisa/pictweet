34aa6b2a6e2e420618ae4890f782d4c65590e6b7
toda-takahisa <todatti2533@gmail.com>
2018-07-09 15:00:52 +0900

    first

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+
+	def index
+		@tweets = Tweet.all
+	end
+
+	def new
+		
+	end
+
+	def create
+		Tweet.create(name: "", image: "", text: "")
+	end
+end
